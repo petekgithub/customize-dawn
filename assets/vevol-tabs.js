35ce213cd1f93ff3179312f9f4e004092254ac14
Petech <petekshamitbeyli@gmail.com>
2024-02-06 01:01:21 +0300

    js-tabs-update3

--- assets/vevol-tabs.js
+++ assets/vevol-tabs.js
@@ -1,29 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const tabButtons = document.querySelectorAll('.tab-button');
-  const tabContents = document.querySelectorAll('.inner-content');
-
-  // İlk tabı ve içeriğini aktif yap
-  tabButtons[0].classList.add('active');
-  tabButtons[0].style.backgroundColor = 'white';
-  tabContents[0].classList.add('active');
-
-  tabButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      // Tüm içerikleri gizle
-      tabContents.forEach((content) => {
-        content.classList.remove('active');
-      });
-
-      // Tüm tab butonlarının aktif durumlarını kaldır
-      tabButtons.forEach((btn) => {
-        btn.classList.remove('active');
-        btn.style.backgroundColor = ''; // Tüm tab butonlarının arka plan rengini temizle
-      });
-
-      // Tıklanan tab butonunu ve içeriğini aktif hale getir
-      button.classList.add('active');
-      button.style.backgroundColor = 'white'; // Tıklanan tabın arka plan rengini beyaz yap
-      tabContents[index].classList.add('active');
-    });
+function showTabContent(tab) {
+  // Tüm tab butonlarının aktiflik özelliğini kaldır
+  let tabButtons = document.querySelectorAll('.tab-button');
+  tabButtons.forEach(function (button) {
+    button.classList.remove('active');
   });
-});
+
+  // Tüm tab içeriklerini gizle
+  let tabContents = document.querySelectorAll('.content');
+  tabContents.forEach(function (content) {
+    content.style.display = 'none';
+  });
+
+  // Tıklanan tab butonunu aktif yap
+  tab.classList.add('active');
+
+  // İlgili tab içeriğini göster
+  let tabContentId = tab.getAttribute('data-tab');
+  let tabContent = document.querySelector('.content[data-tab="' + tabContentId + '"]');
+  tabContent.style.display = 'block';
+}
