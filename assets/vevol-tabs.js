19866c70e90b9d8af6f9c46a01172a3ab49cb923
Petech <petekshamitbeyli@gmail.com>
2024-02-06 00:39:45 +0300

    js-update2

--- assets/vevol-tabs.js
+++ assets/vevol-tabs.js
@@ -2,26 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
   const tabButtons = document.querySelectorAll('.tab-button');
   const tabContents = document.querySelectorAll('.inner-content');
 
+  function showTabContent(tabNum) {
+    tabContents.forEach((content) => {
+      content.classList.remove('active');
+    });
+    document.querySelector(`.inner-content[data-tab="${tabNum}"]`).classList.add('active');
+  }
+
   tabButtons.forEach((button) => {
     button.addEventListener('click', () => {
       const tabNum = button.dataset.tab;
+      console.log('click yapıldı: ' + tabNum);
 
       // Deactivate all tab buttons and content
-      tabButtons.forEach((btn) => btn.classList.remove('active'));
-      tabContents.forEach((content) => content.classList.remove('active'));
-
-      // Activate the clicked tab button and content
-      button.classList.add('active');
-      document.querySelector(`.inner-content[data-tab="${tabNum}"]`).classList.add('active');
-
-      // Change background color of active button
       tabButtons.forEach((btn) => {
-        if (btn.classList.contains('active')) {
-          btn.style.backgroundColor = '#ffffff'; // Set background color to white
-        } else {
-          btn.style.backgroundColor = ''; // Reset background color
-        }
+        btn.classList.remove('active');
+        btn.style.backgroundColor = ''; // Reset background color
       });
+
+      // Activate the clicked tab button
+      button.classList.add('active');
+      button.style.backgroundColor = '#ffffff'; // Set background color to white
+
+      // Show the corresponding tab content
+      showTabContent(tabNum);
     });
   });
 });
