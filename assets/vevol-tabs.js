ca7b1183ae0a586e06fe6d1b6bf9220e09f4b699
Petech <petekshamitbeyli@gmail.com>
2024-02-06 01:27:56 +0300

    final

--- assets/vevol-tabs.js
+++ assets/vevol-tabs.js
@@ -1,21 +1,24 @@
 function showTabContent(tab) {
+  let tabIndex = tab.getAttribute('data-tab');
+
+  // Tüm tab butonlarının aktiflik özelliğini kaldır
   let tabButtons = document.querySelectorAll('.tab-button');
   tabButtons.forEach(function (button) {
     button.classList.remove('active');
   });
 
+  // Tüm tab içeriklerini gizle
   let tabContents = document.querySelectorAll('.content');
   tabContents.forEach(function (content) {
     content.style.display = 'none';
   });
 
-  let selectedTabButton = document.querySelector('.tab-button:nth-child(' + (index + 1) + ')');
-  if (selectedTabButton) {
-    selectedTabButton.classList.add('active');
-  }
+  // Tıklanan tab butonunu aktif yap
+  tab.classList.add('active');
 
-  let selectedTabContent = document.querySelector('.content:nth-child(' + (index + 1) + ')');
-  if (selectedTabContent) {
-    selectedTabContent.style.display = 'block';
+  // İlgili tab içeriğini göster
+  let tabContent = document.querySelector('.content[data-tab="' + tabIndex + '"]');
+  if (tabContent) {
+    tabContent.style.display = 'block'; // İlgili tab içeriğini göster
   }
 }
