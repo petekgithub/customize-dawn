ea1d3f07400702777b82376e5825d606e86a210f
Petech <petekshamitbeyli@gmail.com>
2024-02-06 00:26:07 +0300

    clickable

--- assets/vevol-tabs.js
+++ assets/vevol-tabs.js
@@ -15,4 +15,19 @@ document.addEventListener('DOMContentLoaded', function () {
       document.querySelector(`.content[data-tab="${tabNum}"]`).classList.add('active');
     });
   });
+
+  // Add click event listeners to tab content for each tab
+  tabContents.forEach((content) => {
+    content.addEventListener('click', () => {
+      const tabNum = content.dataset.tab;
+
+      // Deactivate all tab buttons and content
+      tabButtons.forEach((btn) => btn.classList.remove('active'));
+      tabContents.forEach((content) => content.classList.remove('active'));
+
+      // Activate the corresponding tab button and content
+      document.querySelector(`.tab-button[data-tab="${tabNum}"]`).classList.add('active');
+      content.classList.add('active');
+    });
+  });
 });
