bdd405c85980a67eaa297926415166a94f192b90
Petech <petekshamitbeyli@gmail.com>
2024-02-06 00:47:57 +0300

    js-update4

--- assets/vevol-tabs.js
+++ assets/vevol-tabs.js
@@ -2,29 +2,23 @@ document.addEventListener('DOMContentLoaded', function () {
   const tabButtons = document.querySelectorAll('.tab-button');
   const tabContents = document.querySelectorAll('.inner-content');
 
-  function showTabContent(tabNum) {
-    tabContents.forEach((content) => {
-      content.classList.remove('active');
-    });
-    document.querySelector(`.inner-content[data-tab="${tabNum}"]`).classList.add('active');
-  }
-
-  tabButtons.forEach((button) => {
+  tabButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-      const tabNum = button.dataset.tab;
-
-      // Deactivate all tab buttons and content
-      tabButtons.forEach((btn) => {
-        btn.classList.remove('active');
-        btn.style.backgroundColor = ''; // Reset background color
+      // Tüm içerikleri gizle
+      tabContents.forEach((content) => {
+        content.classList.remove('active');
       });
 
-      // Activate the clicked tab button
-      button.classList.add('active');
-      button.style.backgroundColor = '#ffffff'; // Set background color to white
+      // Tüm tab butonlarının aktif durumlarını kaldır
+      tabButtons.forEach((btn) => {
+        btn.classList.remove('active');
+      });
 
-      // Show the corresponding tab content
-      showTabContent(tabNum);
+      // Tıklanan tab butonunu aktif hale getir
+      button.classList.add('active');
+
+      // İlgili içeriği göster
+      tabContents[index].classList.add('active');
     });
   });
 });
