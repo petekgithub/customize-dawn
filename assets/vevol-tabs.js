1c3abda60e4457e525ac8a01eda56e38c74cd7f1
Petech <petekshamitbeyli@gmail.com>
2024-02-06 00:29:54 +0300

    clickable-update

--- assets/vevol-tabs.js
+++ assets/vevol-tabs.js
@@ -1,33 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const tabButtons = document.querySelectorAll('.tab-button');
+function showTabContent(tabButton) {
+  // Deactivate all tab buttons
+  const allTabButtons = document.querySelectorAll('.tab-button');
+  allTabButtons.forEach((button) => button.classList.remove('active'));
+
+  // Activate the clicked tab button
+  tabButton.classList.add('active');
+
+  // Hide all tab contents
+  const allContents = document.querySelectorAll('.content');
+  allContents.forEach((content) => (content.style.display = 'none'));
+
+  // Show the corresponding tab content
+  const index = Array.from(tabButton.parentNode.children).indexOf(tabButton);
   const tabContents = document.querySelectorAll('.content');
-
-  tabButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const tabNum = button.dataset.tab;
-
-      // Deactivate all tab buttons and content
-      tabButtons.forEach((btn) => btn.classList.remove('active'));
-      tabContents.forEach((content) => content.classList.remove('active'));
-
-      // Activate the clicked tab button and content
-      button.classList.add('active');
-      document.querySelector(`.content[data-tab="${tabNum}"]`).classList.add('active');
-    });
-  });
-
-  // Add click event listeners to tab content for each tab
-  tabContents.forEach((content) => {
-    content.addEventListener('click', () => {
-      const tabNum = content.dataset.tab;
-
-      // Deactivate all tab buttons and content
-      tabButtons.forEach((btn) => btn.classList.remove('active'));
-      tabContents.forEach((content) => content.classList.remove('active'));
-
-      // Activate the corresponding tab button and content
-      document.querySelector(`.tab-button[data-tab="${tabNum}"]`).classList.add('active');
-      content.classList.add('active');
-    });
-  });
-});
+  tabContents[index].style.display = 'block';
+}
